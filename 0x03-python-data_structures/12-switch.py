42eff6c7f3084a7841b9e2f4f54f42bc889db0fb
David Elman <117977751+Davidelman31@users.noreply.github.com>
2023-08-14 23:24:18 +0300

    Update 12-switch.py

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-first_number = 89
-second_number = 10
-first_number, second_number = second_number, first_number
-print("first_number={:d} - second_number={:d}".format(first_number, second_number))
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
 
