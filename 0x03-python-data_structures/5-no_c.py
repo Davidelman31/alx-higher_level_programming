1ab19922d8bf266ef59a3dc0cb3bfb0301f7679a
David Elman <117977751+Davidelman31@users.noreply.github.com>
2023-08-14 23:44:42 +0300

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@
 
 def no_c(input_string):
     """Remove all occurrences of characters 'c' and 'C' from a string."""
-    filtered_characters = [char for char in input_string if char.lower() != 'c']
-    return "".join(filtered_characters)
+    copy = [char for char in input_string if char.lower() != 'c']
+    return "".join(copy)
